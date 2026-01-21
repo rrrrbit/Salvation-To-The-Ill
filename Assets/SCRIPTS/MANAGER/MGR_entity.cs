eae970a8924db5fc217cf8d6eb987b82c973b656
rrrrbit <158446026+rrrrbit@users.noreply.github.com>
2026-01-21 15:41:24 +0000

    randomisation progress

--- Assets/SCRIPTS/MANAGER/MGR_entity.cs
+++ Assets/SCRIPTS/MANAGER/MGR_entity.cs
@@ -1,13 +1,16 @@
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class MGR_entity : MonoBehaviour
 {
     public GameObject pickupPrefab;
     public GameObject npcPrefab;
 
-
+    public RandomEntitySettings spawnSettings;
 
     public List<ENTITY> entities = new();
     public float recalcPathsTime = .1f;
@@ -27,6 +30,75 @@ public class MGR_entity : MonoBehaviour
             {
                 ((NPC_movement)npc.movement).RecalcPath();
             }
+        }
+    }
+
+    public void createRandomNpc()
+    {
+        var thisNpc = RandomNPC();
+        thisNpc.transform.position = new(0,3,0);
+    }
+
+    GameObject RandomNPC()
+    {
+        var thisNpc = Instantiate(npcPrefab);
+        var chanceOfArmed = MGR.game.difficulty * CountTeam(ENTITY.Teams.HUMAN) / 
+            (Mathf.Sqrt(CountTeam(ENTITY.Teams.ZOMBIE) + 1) + MGR.game.difficulty * CountTeam(ENTITY.Teams.HUMAN));
+        var health = RandAttrOverDiff(spawnSettings.minHealth, spawnSettings.maxHealth, spawnSettings.healthSkew);
+        var size = RandAttrOverDiff(spawnSettings.minSize, spawnSettings.maxSize, spawnSettings.sizeSkew);
+        var speed = RandAttrOverDiff(spawnSettings.minSpeed, spawnSettings.maxSpeed, spawnSettings.speedSkew);
+        var defense = RandAttrOverDiff(spawnSettings.minDefense, spawnSettings.maxDefense, spawnSettings.defenseSkew);
+        var convResistance = RandAttrOverDiff(spawnSettings.minConvResistance, spawnSettings.maxConvResistance, spawnSettings.convResistanceSkew);
+
+        var item = spawnSettings.items[RandomIndex(spawnSettings.itemChances.Select(x => x.Evaluate(MGR.game.difficulty)).ToList())];
+        var quality = (ItemData.Qualities)RandomIndex(spawnSettings.qualityChances.Select(x => x.Evaluate(MGR.game.difficulty)).ToList());
+
+        var stats = thisNpc.GetComponent<Stats>();
+        stats.maxHealth = health;
+        stats.health = health;
+        stats.defense = defense;
+        stats.convResistance = convResistance;
+        stats.speed = speed;
+        stats.size = size;
+        var inv = thisNpc.GetComponent<Inventory>();
+
+        if(Random.value <= chanceOfArmed)
+        {
+            var thisItem = Instantiate(item, MGR.entities.itemParents);
+            inv.inventory[0] = thisItem.GetComponent<ItemData>();
+            inv.inventory[0].quality = quality;
+        }
+
+        return thisNpc;
+    }
+
+    float RandAttrOverDiff(AnimationCurve min, AnimationCurve max, AnimationCurve curve) => Mathf.Lerp(min.Evaluate(MGR.game.difficulty), max.Evaluate(MGR.game.difficulty), curve.Evaluate(Random.value));
+    int CountTeam(ENTITY.Teams team) => entities.Where(x => x.team == team).Count();
+    int RandomIndex(List<float> chances)
+    {
+        var val = Random.Range(0, chances.Sum());
+        var place = 0f;
+        var i = 0;
+        for (i = 0; i < chances.Count(); i++)
+        {
+            place += chances[i];
+            if (place >= val) break;
+        }
+        return i;
+    }
+}
+
+[CustomEditor(typeof(MGR_entity))]
+public class FAWK : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        MGR_entity myScript = (MGR_entity)target;
+        if (GUILayout.Button("Build Object"))
+        {
+            myScript.createRandomNpc();
         }
     }
 }
--- Assets/SCRIPTS/RandomEntitySettings.cs
+++ Assets/SCRIPTS/RandomEntitySettings.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Random Entity Settings", menuName = "Game/Random Entity Settings", order = 3)]
 public class RandomEntitySettings : ScriptableObject
 {
+    public GameObject[] items;
+    public AnimationCurve[] itemChances;
+    [Space] 
     public AnimationCurve[] qualityChances;
     [Space]
     public AnimationCurve minSpeed;
@@ -13,8 +16,16 @@ public class RandomEntitySettings : ScriptableObject
     public AnimationCurve maxSize;
     public AnimationCurve sizeSkew;
     [Space]
+    public AnimationCurve minHealth;
+    public AnimationCurve maxHealth;
     public AnimationCurve healthSkew;
+    [Space]
+    public AnimationCurve minDefense;
+    public AnimationCurve maxDefense;
     public AnimationCurve defenseSkew;
+    [Space]
+    public AnimationCurve minConvResistance;
+    public AnimationCurve maxConvResistance;
     public AnimationCurve convResistanceSkew;
     [Space]
     public Gradient zombieColours;
