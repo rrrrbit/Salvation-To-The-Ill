623a6a71fda2f95c19e15eb13dbf5e2aac112b11
rrrrbit <158446026+rrrrbit@users.noreply.github.com>
2026-01-28 15:41:55 +0000

    hit particle start

--- Assets/SCRIPTS/MANAGER/MGR_vfx.cs
+++ Assets/SCRIPTS/MANAGER/MGR_vfx.cs
@@ -47,6 +47,11 @@ public class MGR_vfx : MonoBehaviour
 		var thisExpl = Instantiate(heal?healExplosion:explosion, pos.position, new());
 		VFX_explosion expl = thisExpl.GetComponent<VFX_explosion>();
 		expl.range = range*4;
-		expl.speed = 15;
+		expl.speed = 20;
+	}
+
+	public void BulletPtcl(Vector3 velocity, Vector3 hitNormal)
+	{
+
 	}
 }
